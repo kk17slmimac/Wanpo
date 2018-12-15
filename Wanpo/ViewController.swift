e5e69e16986251e9beef827973439bf5df1881fd
久保田慧 <[kk17slmimac@gmail.com]>
2018-12-15 11:24:55 +0900

    Initial Commit

---
+++ Wanpo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wanpo
+//
+//  Created by 久保田慧 on 2018/12/15.
+//  Copyright © 2018年 KeiKubota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
